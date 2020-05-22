99ef82fce7e9ad6abd76c65172735ff916e9b170
Hamza Yousuf <hamzayousuf121@gmail.com>
2020-05-22 22:00:19 +0500

    Monster Rolodex

---
+++ precache-manifest.24ed0e17e6e366050a30f6588dca1228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30049cb977d66f2c08e8767e8d303808",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "539fb57ab1145ba121d7",
+    "url": "/react-monster-rolodex/static/css/main.f50518b7.chunk.css"
+  },
+  {
+    "revision": "d7732017bf35d5f1f230",
+    "url": "/react-monster-rolodex/static/js/2.1112e5bb.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/react-monster-rolodex/static/js/2.1112e5bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "539fb57ab1145ba121d7",
+    "url": "/react-monster-rolodex/static/js/main.9f2c25b5.chunk.js"
+  },
+  {
+    "revision": "0f5d8c34438559d8800b",
+    "url": "/react-monster-rolodex/static/js/runtime-main.bb9238ca.js"
+  }
+]);
